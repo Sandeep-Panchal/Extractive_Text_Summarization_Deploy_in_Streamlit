1bc146795f03c1b9a414aecaee0711d336210aaa
Sandeep-Panchal <42773950+Sandeep-Panchal@users.noreply.github.com>
2021-11-02 19:50:13 +0530

    Add files via upload

---
+++ extractive_text_summarization_ui.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import tensorflow_hub as hub
+import tensorflow as tf
+
+# load the universal sentence encoder model from tf hub
+def load_use_model():
+
+    # loads the universal sentence encoder model
+    use_model = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+    # use_model = hub.load('https://tfhub.dev/google/universal-sentence-encoder-large/5')
+    return use_model
+
+# getting the embedding of the text (query from user)
+def embed(text, mod):
+    return np.array(mod(text))
+
+@st.cache(suppress_st_warning=True)
+def load_all_fun():
+
+    # call load_use_model to load the universal sentence encoder model from tf hub
+    use_model = load_use_model()
+
+    return use_model
+
+query = 'aws, s3bucket, ec2'
+def vectorize(query):
+
+    query_vect = embed([query], use_model)
+
+    st.write(query_vect)
+
+if __name__ == '__main__':
+
+    use_model = load_all_fun()
+    vectorize(query)
+
