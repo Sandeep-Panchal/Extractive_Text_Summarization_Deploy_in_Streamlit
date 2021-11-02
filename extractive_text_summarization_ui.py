9aec4b23228d03964062c16aa5f45fa844d80a81
Sandeep-Panchal <42773950+Sandeep-Panchal@users.noreply.github.com>
2021-11-02 22:39:50 +0530

    Update extractive_text_summarization_ui.py

--- extractive_text_summarization_ui.py
+++ extractive_text_summarization_ui.py
@@ -8,7 +8,10 @@ st.set_page_config(page_title='Extractive Text Summarization', page_icon=':smile
 # UI starts from here
 st.title('Extractive Text Summarization')
 st.write('')
+st.write('__*Text summarization*__ is a task that involves extracting the summary of the whole content while preserving the key information.')
+st.write('Unlike __*Abstractive Summarization*__ that produces the whole new phrases which may not be present in the given content, __*Extractive Summarization*__ produces the summary by extracting the phrases from the given content. Extracted phrases are from the given content itself.')
 
+st.write('')
 query = st.text_area("Please enter your text content...", height=300)
 submit_button = st.button("Submit")
 
